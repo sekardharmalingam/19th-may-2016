de57de3b02f6a418186da5370993388b4b5601e3
Dharmalingam <sekar.dharmalingam@capgemini.com>
2016-07-14 17:10:47 +0530

    Sekar

---
+++ MyFirstWebApplication/src/com/capgemini/mypack/MyFirstServlet.java
@@ -0,0 +1,48 @@
+package com.capgemini.mypack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello Servlet on console");
+		
+		PrintWriter out = response.getWriter();//sending text to the client(browser)
+		
+		out.println("Hello Servlet");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
